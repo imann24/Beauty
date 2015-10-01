31b3c483a0655891a8aab592dd78315efb59ed35
imann24 <imann24@gmail.com>
2015-10-01 17:14:18 -0400

    Adding updated audio track

--- Unity_Project/Assets/Scripts/MessageController.cs
+++ Unity_Project/Assets/Scripts/MessageController.cs
@@ -2,16 +2,23 @@
 using System.Collections;
 
 public class MessageController : MonoBehaviour {
+	public static MessageController Instance;
 	public GameObject TopOfScreenMessage;
 	public enum Screen {Top, Bottom, Left, Right, Center};
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void SetMessage (Screen screenLocation) {
+		if (screenLocation == Screen.Top) {
+			//TopOfScreenMessage
+		}
 	}
 }
